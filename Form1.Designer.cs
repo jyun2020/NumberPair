f3cac069e4d2a4b4395d25d821826ee9a25726f9
jyun2020 <jyun2020@users.noreply.github.com>
2021-03-03 18:43:30 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace NumberPair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Easy = new System.Windows.Forms.RadioButton();
+            this.normal = new System.Windows.Forms.RadioButton();
+            this.God = new System.Windows.Forms.RadioButton();
+            this.Start = new System.Windows.Forms.Button();
+            this.hard = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("splitContainer1.Panel1.BackgroundImage")));
+            this.splitContainer1.Panel1.Controls.Add(this.God);
+            this.splitContainer1.Panel1.Controls.Add(this.Start);
+            this.splitContainer1.Panel1.Controls.Add(this.hard);
+            this.splitContainer1.Panel1.Controls.Add(this.normal);
+            this.splitContainer1.Panel1.Controls.Add(this.Easy);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("splitContainer1.Panel2.BackgroundImage")));
+            this.splitContainer1.Size = new System.Drawing.Size(784, 561);
+            this.splitContainer1.SplitterDistance = 170;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Easy
+            // 
+            this.Easy.AutoSize = true;
+            this.Easy.Checked = true;
+            this.Easy.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Easy.Location = new System.Drawing.Point(30, 24);
+            this.Easy.Margin = new System.Windows.Forms.Padding(2);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(104, 28);
+            this.Easy.TabIndex = 1;
+            this.Easy.TabStop = true;
+            this.Easy.Text = "菜雞難度";
+            this.Easy.UseVisualStyleBackColor = true;
+            // 
+            // normal
+            // 
+            this.normal.AutoSize = true;
+            this.normal.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.normal.Location = new System.Drawing.Point(30, 56);
+            this.normal.Margin = new System.Windows.Forms.Padding(2);
+            this.normal.Name = "normal";
+            this.normal.Size = new System.Drawing.Size(104, 28);
+            this.normal.TabIndex = 2;
+            this.normal.Text = "中級難度";
+            this.normal.UseVisualStyleBackColor = true;
+            // 
+            // God
+            // 
+            this.God.AutoSize = true;
+            this.God.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.God.Location = new System.Drawing.Point(30, 120);
+            this.God.Margin = new System.Windows.Forms.Padding(2);
+            this.God.Name = "God";
+            this.God.Size = new System.Drawing.Size(104, 28);
+            this.God.TabIndex = 7;
+            this.God.Text = "上帝模式";
+            this.God.UseVisualStyleBackColor = true;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(30, 152);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(104, 58);
+            this.Start.TabIndex = 6;
+            this.Start.Text = "開始遊戲";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // hard
+            // 
+            this.hard.AutoSize = true;
+            this.hard.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.hard.Location = new System.Drawing.Point(30, 88);
+            this.hard.Margin = new System.Windows.Forms.Padding(2);
+            this.hard.Name = "hard";
+            this.hard.Size = new System.Drawing.Size(104, 28);
+            this.hard.TabIndex = 5;
+            this.hard.Text = "高級難度";
+            this.hard.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RadioButton God;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.RadioButton hard;
+        private System.Windows.Forms.RadioButton normal;
+        private System.Windows.Forms.RadioButton Easy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberPair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random(); //新增亂數物件
+        List<int> number = new List<int>(); //新增按鈕數值串列
+        Button FirstClickButton = null;  //暫存第一次按的button
+        int Ranks;  //難度變數
+        int Clicktimes;  //紀錄按第幾次,按完兩次要歸零
+        int WinJudge;  //紀錄有沒有全對
+        int StepCount; //紀錄步數
+        
+        private void Start_Click(object sender, EventArgs e) //開始遊戲
+        {   //判斷radiobutton並設定難度為幾成幾
+            //設定完開始遊戲布局
+            Clicktimes = 0;
+            WinJudge = 0;
+            StepCount = 0;
+            if (Easy.Checked)
+            {
+                Ranks = 4;
+                NewGame(Ranks);
+            }
+            if (normal.Checked)
+            {
+                Ranks = 6;
+                NewGame(Ranks);
+            }
+            if (hard.Checked)
+            {
+                Ranks = 8;
+                NewGame(Ranks);
+            }
+            if (God.Checked)
+            {
+                Ranks = 10;
+                NewGame(Ranks);
+            }
+        }
+        private void NewGame(int Ranks)
+        {
+            this.splitContainer1.Panel2.Controls.Clear();
+            number.Clear();
+            for (int i = 1; i <= Ranks * Ranks / 2; i++)
+            {
+                number.Add(i);
+                number.Add(i);
+                //如果4*4代表串列要放兩組(1~8)...以此類推
+            }
+            //開始新增按鈕並設定
+            for (int i = 0; i < Ranks; i++)
+            {
+                for (int j = 0; j < Ranks; j++)
+                {
+                    int rd = r.Next(0, number.Count);//隨機串列中的個數
+                    Button button = new Button()
+                    {
+                        Text = number[rd].ToString(),//取出串列中的值轉成字串,設定成button的text
+                        Size = new Size(40, 40),//設定大小
+                        Location = new Point(50 * i, 50 * j),//設定擺放位置
+                        BackColor = Color.Black //用顏色設定為隱藏
+                    };
+                    button.Click += new EventHandler(button_Click);//設定按鈕共用一個click事件
+                    number.RemoveAt(rd);//設定完一個button就從串列中移除該值
+                    this.splitContainer1.Panel2.Controls.Add(button);//把做好的按鈕加進Panel2
+                }
+            }
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Clicktimes += 1;//每次按都先+1
+            if (Clicktimes == 1)//如果是第一次翻牌
+            {
+                btn.BackColor = Color.White;//顯示
+                btn.Enabled = false;//不可再選
+                FirstClickButton = btn;//把第一次按鈕暫存
+                StepCount += 1;//步數+1
+            }
+            else if (Clicktimes == 2)//如果是第二次翻牌
+            {
+                Clicktimes = 0;//重設點擊次數
+                if (FirstClickButton.Text == btn.Text)//如果翻到一樣的
+                {
+                    WinJudge += 2;//勝利判斷值+2
+                    btn.BackColor = Color.White;//顯示
+                    btn.Enabled = false;//不可再選
+                    FirstClickButton = null;//重設暫存按鈕
+                    if (WinJudge == Ranks * Ranks)
+                    {
+                        MessageBox.Show("全對!總共點擊了" + StepCount + "次,您真牛逼");
+                    }
+                }
+                else//如果翻到不一樣的
+                {
+                    btn.BackColor = Color.White;//顯示
+                    btn.Enabled = false;//不可選
+                    this.Refresh();//更新畫面
+                    Thread.Sleep(500);//暫停0.5秒
+                    btn.BackColor = Color.Black;//隱藏
+                    btn.Enabled = true;//可選
+                    FirstClickButton.BackColor = Color.Black;//將第一次按的也隱藏
+                    FirstClickButton.Enabled = true;//可選
+                    FirstClickButton = null;//重設暫存按鈕
+                }
+            }
+        }
+    }
+}
+
